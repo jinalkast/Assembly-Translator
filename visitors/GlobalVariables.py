7b9c33329c06099093026ff72a3333ae118fb8a1
Jinal <kasturij@mcmaster.ca>
2022-11-22 18:01:44 -0500

    optimized global var extraction

--- visitors/GlobalVariables.py
+++ visitors/GlobalVariables.py
@@ -13,34 +13,45 @@ class GlobalVariableExtraction(ast.NodeVisitor):
         if len(node.targets) != 1:
             raise ValueError("Only unary assignments are supported")
         
-        # if node value == constant pass in value as well
-        if isinstance(node.value, ast.Constant):
-            self.results[node.targets[0].id] = node.value.value
-        
-        elif node.targets[0].id not in self.results:
-            if isinstance(node.value, ast.BinOp):
-                left = None
-                right = None 
-
-                if isinstance(node.value.left, ast.Name):
-                    if node.value.left.id in self.results:
-                        left = self.results[node.value.left.id]
-                else:
-                    left = node.value.left.value
-
-                if isinstance(node.value.right, ast.Name):
-                    if node.value.right.id in self.results:
-                        right = self.results[node.value.right.id]
-                else:
-                    right = node.value.right.value
-
-                if left != None and right != None:
-                    if isinstance(node.value.op, ast.Add):
-                        self.results[node.targets[0].id] = left + right
-                    else:
-                        self.results[node.targets[0].id] = left - right
+        if node.targets[0].id not in self.results:
+        # if node value is constant, pass value into result
+            if isinstance(node.value, ast.Constant):
+                self.results[node.targets[0].id] = node.value.value
+            
+            #if node value not constant
             else:
-                self.results[node.targets[0].id] = None
+                # if the variable is assigned to a binary operation, try and compute its value for optimization
+                if isinstance(node.value, ast.BinOp):
+                    left = None
+                    right = None 
+
+                    # if the left element is a known variable, copy the value from its assignment
+                    if isinstance(node.value.left, ast.Name):
+                        if node.value.left.id in self.results:
+                            left = self.results[node.value.left.id]
+                    # if the left element is not a variable name, it's a constant - copy its value
+                    else:
+                        left = node.value.left.value
+
+                    #same for right element
+                    if isinstance(node.value.right, ast.Name):
+                        if node.value.right.id in self.results:
+                            right = self.results[node.value.right.id]
+                    else:
+                        right = node.value.right.value
+
+                    # if both elements are statically known at compilation, perform the binOp and pass the value into the result - otherwise, pass None
+                    if left != None and right != None:
+                        if isinstance(node.value.op, ast.Add):
+                            self.results[node.targets[0].id] = left + right
+                        else:
+                            self.results[node.targets[0].id] = left - right
+                    else:
+                        self.results[node.targets[0].id] = None
+
+                # pass None if the variable is not assigned to binOp
+                else:
+                    self.results[node.targets[0].id] = None
 
 
     def visit_FunctionDef(self, node):
