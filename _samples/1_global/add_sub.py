d45ab2807122c4bb980cb33e2b9c079159d24619
Jinal <kasturij@mcmaster.ca>
2022-12-08 13:24:07 -0500

    updated add_sub

--- _samples/1_global/add_sub.py
+++ _samples/1_global/add_sub.py
@@ -1,6 +1,6 @@
-eeeeeeeeeeeeeeeeeeeeeeeee = int(input())
+value = int(input())
 _UNIV = 42
-result = eeeeeeeeeeeeeeeeeeeeeeeee + _UNIV
+result = value + _UNIV
 variable = 3
 result = result - variable
 result = result - 1
